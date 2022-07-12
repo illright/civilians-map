3c78c98687c2a1197d99158dcb7a8e350eeaa5ca
Lev Chelyadinov <leva181777@gmail.com>
2022-07-12 17:04:52 +0500

    Make the marker more elegant

--- src/lib/marker.svelte
+++ src/lib/marker.svelte
@@ -2,6 +2,8 @@
 	import { getContext, onMount } from 'svelte';
 	import { mapCtx, type MapContext } from './map.svelte';
 
+	import './marker.css';
+
 	export let position: [number, number];
 	export let iconUrl: string;
 	const context = getContext<MapContext>(mapCtx);
@@ -13,8 +15,9 @@
 				leaflet.marker(position, {
 					icon: leaflet.icon({
 						iconUrl,
-						iconSize: [48, 48]
-					})
+						iconSize: [40, 40],
+						className: 'rounded-full'
+					}),
 				})
 			);
 		});
