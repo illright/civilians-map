a4bbb75d16dead34ff10f759b98562c9336231e1
Lev Chelyadinov <leva181777@gmail.com>
2022-07-12 22:26:38 +0500

    Implement the popup component

--- src/lib/marker.svelte
+++ src/lib/marker.svelte
@@ -1,25 +1,47 @@
 <script lang="ts">
-	import { getContext, onMount } from 'svelte';
+	import { getContext, onMount, SvelteComponentTyped, type ComponentType } from 'svelte';
 	import { mapCtx, type MapContext } from './map.svelte';
 
 	import './marker.css';
 
 	export let position: [number, number];
 	export let iconUrl: string;
+	export let popup: ComponentType | undefined = undefined;
+	export let popupProps: Record<string, unknown> = {};
+
 	const context = getContext<MapContext>(mapCtx);
 
 	onMount(async () => {
 		const leaflet = await import('leaflet');
 		return context.cluster.subscribe((cluster) => {
-			cluster?.addLayer(
-				leaflet.marker(position, {
-					icon: leaflet.icon({
-						iconUrl,
-						iconSize: [40, 40],
-						className: 'rounded-full'
-					}),
+			const marker = leaflet.marker(position, {
+				icon: leaflet.icon({
+					iconUrl,
+					iconSize: [40, 40],
+					className: 'rounded-full'
 				})
-			);
+			});
+
+			// Credit: https://imfeld.dev/writing/leaflet_with_svelte#popups
+			if (popup !== undefined) {
+				let popupComponent: SvelteComponentTyped | undefined = undefined;
+
+				marker.bindPopup(() => {
+					let container = leaflet.DomUtil.create('div');
+					popupComponent = new popup!({ target: container, props: popupProps });
+					return container;
+				}, { offset: [0, -20] });
+
+				marker.on('popupclose', () => {
+					if (popupComponent !== undefined) {
+						let old = popupComponent;
+						popupComponent = undefined;
+						setTimeout(() => old.$destroy(), 500);
+					}
+				});
+			}
+
+			cluster?.addLayer(marker);
 		});
 	});
 </script>
---
+++ src/lib/popup.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import IconTelegram from '~icons/logos/telegram';
+	export let name: string;
+	export let telegram: string;
+	export let location: string | null;
+	export let hobbies: string;
+	export let workInterests: string;
+
+	$: telegramUsername = telegram.replace(/^@/, '');
+</script>
+
+<h1>
+	<span>{name}</span>
+	<a href="https://t.me/{telegramUsername}"
+		><IconTelegram width="20" height="20" />
+		<span class="sr-only">Telegram: {telegramUsername}</span>
+	</a>
+</h1>
+<div class="location">{location}</div>
+<h2>Увлечения</h2>
+<p>{hobbies}</p>
+<h2>Рабочие интересы</h2>
+<p>{workInterests}</p>
+
+<style>
+	h1 {
+		font-size: 1.3em;
+		margin: 1em 0 0.25em;
+		display: flex;
+		align-items: center;
+		gap: 0.5em;
+	}
+
+	h1 :global(svg) {
+		display: block;
+	}
+
+	h2 {
+		font-size: 1.1em;
+		margin: 0.75em 0 0.25em;
+	}
+
+	p {
+		margin: 0 0 0.5em;
+	}
+
+	.location {
+		color: gray;
+	}
+
+	/* Credit: https://stackoverflow.com/a/42665456/5726823 */
+	.sr-only {
+		border: 0 !important;
+		clip: rect(1px, 1px, 1px, 1px) !important;
+		-webkit-clip-path: inset(50%) !important;
+		clip-path: inset(50%) !important;
+		height: 1px !important;
+		margin: -1px !important;
+		overflow: hidden !important;
+		padding: 0 !important;
+		position: absolute !important;
+		width: 1px !important;
+		white-space: nowrap !important;
+	}
+</style>
